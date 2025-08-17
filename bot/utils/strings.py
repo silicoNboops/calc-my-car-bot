d683f5e91fbd1ed1166a39df395240756228c3b8
jojoprison <egyabig2@gmail.com>
2025-08-18 03:26:26 +0700

    Create strings.py

---
+++ bot/utils/strings.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+# Общие текстовые константы и подсказки бота
+
+PROMPT_CHOOSE_CURRENCY = "Выберите, в какой валюте будет указана цена автомобиля:"
+PROMPT_ENTER_PRICE = "Введите стоимость автомобиля (например, 💰 1 200 000):"
+PROMPT_CHOOSE_ROLE = "Кто ввозит автомобиль:"
+PROMPT_CHOOSE_ENGINE_TYPE = "Выберите тип двигателя:"
+PROMPT_ENTER_ENGINE_CC = "Введите объём двигателя в см³ (например, 1500):"
+PROMPT_CHOOSE_AGE = "Выберите возраст автомобиля:"
+
+CONTACT_LINE = "\nСвяжитесь с нами: @Slaford - Слафординка"
