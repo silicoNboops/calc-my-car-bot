8a0463a777d747a87a5d7d30c5eb95a6990e232d
jojoprison <egyabig2@gmail.com>
2025-08-18 02:07:21 +0700

    fix calc router imports

--- bot/routers/__init__.py
+++ bot/routers/__init__.py
@@ -4,9 +4,9 @@ from aiogram import Router
 
 from .start import router as start_router
 from .calculator import router as calculator_router
-from .calculator_wizard import router as calculator_wizard_router
+from .calculator_command import router as calculator_command_router
 
 router = Router()
 router.include_router(start_router)
 router.include_router(calculator_router)
-router.include_router(calculator_wizard_router)
+router.include_router(calculator_command_router)
