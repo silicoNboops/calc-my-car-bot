82e80613db3627ad541adeb30924ff700728f506
jojoprison <egyabig2@gmail.com>
2025-09-02 01:07:27 +0700

    Update utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -5,12 +5,9 @@ def normalize_age_key_for_api(age_key: str, is_jur: bool) -> str:
     """
     Приводит возрастной ключ к тем, что принимает API-сериализатор.
 
-    - Для юрлиц 'over_5' недопустим: заменяем на '5_to_7'.
-    - Для физлиц '5_to_7'/'over_7' нормализуются к 'over_5'.
+    - Легаси 'over_5' заменяем на '5_to_7'.
     - Остальные значения возвращаются без изменений.
     """
-    if is_jur and age_key == "over_5":
+    if age_key == "over_5":
         return "5_to_7"
-    if not is_jur and age_key in {"5_to_7", "over_7"}:
-        return "over_5"
     return age_key
